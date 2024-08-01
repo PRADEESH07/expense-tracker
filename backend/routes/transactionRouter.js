83a63624e2d79c5097dcf9756ec82b7042e01ca6
PRADEESH07 <123649391+PRADEESH07@users.noreply.github.com>
2024-08-01 10:23:48 +0530

    Update transactionRouter.js
    ROUTES UPDATED

--- backend/routes/transactionRouter.js
+++ backend/routes/transactionRouter.js
@@ -3,11 +3,11 @@ const transactionController = require('../controllers/transactionsCtrl');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const transactionRouter= express.Router();
 //!register
-transactionRouter.post("/api/v1/transaction/create",isAuthenticated,transactionController.create)
+transactionRouter.post("/transaction/create",isAuthenticated,transactionController.create)
 //!filter
-transactionRouter.get("/api/v1/transaction/list",isAuthenticated,transactionController.getFilteredTransaction)
+transactionRouter.get("/transaction/list",isAuthenticated,transactionController.getFilteredTransaction)
 //!update
-transactionRouter.put("/api/v1/transaction/update/:id",isAuthenticated,transactionController.update)
+transactionRouter.put("/transaction/update/:id",isAuthenticated,transactionController.update)
 //!delete
-transactionRouter.delete("/api/v1/transaction/delete/:id",isAuthenticated,transactionController.delete)
+transactionRouter.delete("/transaction/delete/:id",isAuthenticated,transactionController.delete)
 module.exports=transactionRouter;
