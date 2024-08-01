742edbafef63c0b7b3f6afb38adcc34198423f4e
PRADEESH07 <123649391+PRADEESH07@users.noreply.github.com>
2024-08-01 10:24:39 +0530

    Update categoriesRouter.js
    ROUTES UPDATED

--- backend/routes/categoriesRouter.js
+++ backend/routes/categoriesRouter.js
@@ -3,11 +3,11 @@ const categoriesController = require('../controllers/categoriesCtrl');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const categoriesRouter= express.Router();
 //!create
-categoriesRouter.post("/api/v1/categories/create",isAuthenticated,categoriesController.create)
+categoriesRouter.post("/categories/create",isAuthenticated,categoriesController.create)
 //!login
-categoriesRouter.get("/api/v1/categories/list",isAuthenticated,categoriesController.list)
+categoriesRouter.get("/categories/list",isAuthenticated,categoriesController.list)
 //!update
-categoriesRouter.put("/api/v1/categories/update/:id",isAuthenticated,categoriesController.update)
+categoriesRouter.put("/categories/update/:id",isAuthenticated,categoriesController.update)
 //!delete
-categoriesRouter.delete("/api/v1/categories/delete/:id",isAuthenticated,categoriesController.delete)
+categoriesRouter.delete("/categories/delete/:id",isAuthenticated,categoriesController.delete)
 module.exports=categoriesRouter;
