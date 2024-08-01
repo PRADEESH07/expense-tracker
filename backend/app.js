fe83a633a2e0f1e418bf9bb04394ced6dda995ff
PRADEESH07 <123649391+PRADEESH07@users.noreply.github.com>
2024-08-01 09:29:39 +0530

    Update app.js
    mongodb  server timeout updated

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const categoriesRouter = require('./routes/categoriesRouter');
 const transactionRouter = require('./routes/transactionRouter')
 const app=express();
 //!connect mongoose
-mongoose.connect("mongodb+srv://pradeesh0702:Pradeesh0207@cluster0.th4v6n1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{console.log("DB connect");}).catch((e)=>{console.log(e);})
+mongoose.connect("mongodb+srv://pradeesh0702:Pradeesh0207@cluster0.th4v6n1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{serverSelectionTimeoutMS: 30000}).then(()=>{console.log("DB connect");}).catch((e)=>{console.log(e);})
 //!cors config
 const corsOptions={
     origin: ["https://expense-tracker-1-5wom.onrender.com"]
