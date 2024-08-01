163a8204c1805a035f91977eb2faef4159937409
PRADEESH07 <123649391+PRADEESH07@users.noreply.github.com>
2024-08-01 09:09:58 +0530

    Update userRouter.js
    routes updated

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,13 +3,13 @@ const usersController = require('../controllers/usersCtrl');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const userRouter= express.Router();
 //!register
-userRouter.post("/api/v1/user/register",usersController.register)
+userRouter.post("/user/register",usersController.register)
 //!login
-userRouter.post("/api/v1/user/login",usersController.login)
+userRouter.post("/user/login",usersController.login)
 //!profile
-userRouter.get("/api/v1/user/profile",isAuthenticated,usersController.profile)
+userRouter.get("/user/profile",isAuthenticated,usersController.profile)
 //!change password
-userRouter.put("/api/v1/user/changePassword",isAuthenticated,usersController.changeUserPassword)
+userRouter.put("/user/changePassword",isAuthenticated,usersController.changeUserPassword)
 //!update user
-userRouter.put("/api/v1/user/updateProfile",isAuthenticated,usersController.updateProfile)
+userRouter.put("/user/updateProfile",isAuthenticated,usersController.updateProfile)
 module.exports=userRouter;
